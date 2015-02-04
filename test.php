8beb073fc9ea4895d497f47468b2f6d984215d89
kr-app-a72 <kr.app.a72@gmail.com>
2015-02-04 12:44:46 +0900

    Create test.php

---
+++ test.php
@@ -0,0 +1,333 @@
+<html>
+<head>
+<title>주석(Comment) - HTML 기본 강좌</title>
+<meta http-equiv="Content-type" content="text/html; charset=euc-kr">
+<link rel="stylesheet" href="/css/main_style.css" type="text/css">
+<link rel="shortcut icon" href="/homejjang.ico">
+
+<script type="text/javascript" src="/syntaxhighlighter_2.1.364/scripts/shCore.js"></script>
+<script type="text/javascript" src="/syntaxhighlighter_2.1.364/scripts/shBrushBash.js"></script>
+<script type="text/javascript" src="/syntaxhighlighter_2.1.364/scripts/shBrushCss.js"></script>
+<script type="text/javascript" src="/syntaxhighlighter_2.1.364/scripts/shBrushJScript.js"></script>
+<script type="text/javascript" src="/syntaxhighlighter_2.1.364/scripts/shBrushPhp.js"></script>
+<script type="text/javascript" src="/syntaxhighlighter_2.1.364/scripts/shBrushSql.js"></script>
+<script type="text/javascript" src="/syntaxhighlighter_2.1.364/scripts/shBrushXml.js"></script>
+<link type="text/css" rel="stylesheet" href="/syntaxhighlighter_2.1.364/styles/shCore.css"/>
+<link type="text/css" rel="stylesheet" href="/syntaxhighlighter_2.1.364/styles/shThemeDefault.css"/>
+<script type="text/javascript">
+	SyntaxHighlighter.config.clipboardSwf = '/syntaxhighlighter_2.1.364/scripts/clipboard.swf';
+	SyntaxHighlighter.all();
+</script>
+
+<!-- google-analytics -->
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-926134-3";
+urchinTracker();
+</script>
+<script type="text/javascript">
+window.google_analytics_uacct = "UA-926134-3";
+</script><script language="JavaScript" type="text/javascript">
+<!--
+function add_naver() {
+	window.open('http://bookmark.naver.com/post?ns=1&ui=popup&url=http://www.homejjang.com/03/comments.php&title='+encodeURIComponent('주석(Comment)'), 'naver_bookmark','width=520,height=365,resize=no'); 
+}
+
+function add_daum() {
+	window.open('http://bookmark.daum.net/bookmark/edit.do?decorator=popup&url=http://www.homejjang.com/03/comments.php&title='+encodeURIComponent('주석(Comment)'), 'pop_bookmark','width=486,height=340');
+}
+
+function add_margarin() {
+	window.open('http://mar.gar.in/post/popup_post/&qTitle='+encodeURIComponent('주석(Comment)')+'&qURL=http://www.homejjang.com/03/comments.php', 'margarin','width=790,height=550,scrollbars=yes,resizable=yes');
+}
+// -->
+</script>
+</head>
+<body bgcolor="#D1D8FF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="935" cellpadding="0" cellspacing="0" border="0" align="center">
+<tr height="20"><td></td></tr>
+<tr>
+	<td bgcolor="#FFFFFF">
+		<table width="935" cellpadding="0" cellspacing="0" border="0" align="center" bgcolor="#FFFFFF">
+		<tr height="20"><td></td></tr>
+		<tr>
+			<td width="180"><a href="/"><img src="/images/logo.png" border="0"></a></td>
+			<td width="15"></td>
+			<td><script type="text/javascript"><!--
+google_ad_client = "pub-5161913013844597";
+/* �덉㎟,B9121B,728x90, �묒꽦�� 09. 11. 24 */
+google_ad_slot = "5700875354";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></td>
+		</tr>
+		<tr height="20"><td></td></tr>
+		</table>
+		<div style="background-color:#FFFFFF;">
+			<ul class="tab01"> 
+							<li class="" id="w70"><p><a href="/index.php">Home</a></p></li>
+							<li class="" id=""><p><a href="/01/homepage.php">홈페이지 기초</a></p></li>
+							<li class="selected" id=""><p><a href="/03/html.php">HTML 기본</a></p></li>
+							<li class="" id=""><p><a href="/05/html2.php">HTML 고급</a></p></li>
+							<li class="" id="w80"><p><a href="/07/css_basic.php">CSS 기본</a></p></li>
+							<li class="" id="w80"><p><a href="/09/css_advance.php">CSS 고급</a></p></li>
+							<li class="" id="w80"><p><a href="/11/web_design.php">웹디자인</a></p></li>
+							<li class="" id="w70"><p><a href="/13/marketing.php">마케팅</a></p></li>
+							<li class="" id="w70"><p><a href="/15/shoppingmall.php">쇼핑몰</a></p></li>
+					</ul>
+		</div>
+	</td>
+</tr>
+</table>
+<table width="935" cellpadding="0" cellspacing="0" border="0" align="center">
+<tr height="5">
+	<td colspan="4"><!-- margin --></td>
+</tr>
+<tr>
+	<td width="5" bgcolor="#FFFFFF"><!-- margin --></td>
+	<td width="180" bgcolor="white" valign="top">
+						<table width="180" cellpadding="0" cellspacing="0" border="0">
+									<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="Tag_element_attribute.php" style="margin-left:0px;color:#000000">HTML의 구성요소</a>
+				</td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="basic_html.php" style="margin-left:0px;color:#000000">HTML 문서의 작성</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="html_structure.php" style="margin-left:0px;color:#000000">HTML의 기본구성</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="body.php" style="margin-left:0px;color:#000000">BODY 태그</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="comments.php" style="margin-left:0px;color:#0E709F">주석(Comment)</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="line_break.php" style="margin-left:0px;color:#000000">줄바꾸기</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="paragraph.php" style="margin-left:0px;color:#000000">P(paragraph)</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="br.php" style="margin-left:0px;color:#000000">BR(Line Break)</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="heading.php" style="margin-left:0px;color:#000000">표제(Heading)</a>
+				</td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="text.php" style="margin-left:0px;color:#000000">텍스트 관련 태그</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="font.php" style="margin-left:0px;color:#000000">FONT 태그</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="Entities.php" style="margin-left:0px;color:#000000">특수문자</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="link.php" style="margin-left:0px;color:#000000">링크(Link)</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="inline_link.php" style="margin-left:0px;color:#000000">내부링크</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="image.php" style="margin-left:0px;color:#000000">이미지(image)</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="img_path.php" style="margin-left:0px;color:#000000">이미지 경로</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="img_format.php" style="margin-left:0px;color:#000000">이미지 포맷</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="img_align.php" style="margin-left:0px;color:#000000">이미지 정렬</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="color.php" style="margin-left:0px;color:#000000">색(color)</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="hex_code.php" style="margin-left:0px;color:#000000">HTML 색상표(1)</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="color_name.php" style="margin-left:0px;color:#000000">HTML 색상표(2)</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="background.php" style="margin-left:0px;color:#000000">배경(background)</a>
+				</td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="table.php" style="margin-left:0px;color:#000000">테이블(table)</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="colspan.php" style="margin-left:0px;color:#000000">열과 행의 확장</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="cellpadding.php" style="margin-left:0px;color:#000000">여백과 경계선</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="w_h.php" style="margin-left:0px;color:#000000">너비와 높이</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="align.php" style="margin-left:0px;color:#000000">정렬</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="frame.php" style="margin-left:0px;color:#000000">프레임(frame)</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="frameset.php" style="margin-left:0px;color:#000000">frameset</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="frame2.php" style="margin-left:0px;color:#000000">frame</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree1.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="frame_target.php" style="margin-left:0px;color:#000000">frame target</a></td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="noframes.php" style="margin-left:0px;color:#000000">noframes</a></td>
+			</tr>
+						<tr height="24">
+				<td colspan="2" style="padding:5px 0px 0px 4px;">
+					<img src="/images/tree00.gif" border="0" width="11" height="11">
+					<a href="html_list.php" style="margin-left:0px;color:#000000">리스트(List)</a>
+				</td>
+			</tr>
+						<tr height="22">
+				<td width="25"><img src="/images/tree2.gif" border="0" width="22" height="22" border="0"></td>
+				<td width="155"><a href="list_type.php" style="margin-left:0px;color:#000000">type</a></td>
+			</tr>
+						</table>	</td>
+	<td width="5"><!-- margin --></td>
+	<td valign="top" width="745">
+		<table width="745" cellpadding="0" cellspacing="0" border="0" align="center" bgcolor="white" align="center">
+						<tr height="30">
+			<td style="font-weight:bold;padding:10px 0px 5px 10px;font-size: 10pt;color: #0E709F; border-bottom:1px solid #DDD;">주석(Comment)</td>
+		</tr>
+		<tr>
+			<td style="padding:10px 10px 0px 20px;">
+				<div style="width:700px;"><P>HTML에서 주석을 사용할려면 &lt;!--&nbsp;와 --&gt; 사이에 내용을 넣으면 됩니다.</P>
+<P>
+<TABLE class=templateTable borderColor=#505050 cellSpacing=0 cellPadding=5 width="90%" align=center border=1>
+<TBODY>
+<TR>
+<TD><FONT color=#ff0000>&lt;!--&nbsp;</FONT>여기에 주석 내용이 옵니다.&nbsp;<FONT color=#ff0000>--&gt;</FONT> </TD></TR></TBODY></TABLE></P>
+<P>주석은 화면상에 표시되지 않고 소스상에서만 확인할 수 있습니다.</P>
+<P>HTML소스를&nbsp;작성할때 문서의 수정사항이나 태그의 설명 등을 보기 편하게 하기위해서 사용되는 것입니다.</P>
+<P>주석을 사용하는 이유는 소스가 아주 길고&nbsp;복잡한 경우 이 소스를 수정할때 알아보기가 힘들기에 주석을 달아놓으면 쉽게 어떤 의도로 이 소스를 작성했는지 알 수 있습니다.</P>
+<P>그리고 여러 사람이 하나의 소스를 공동으로 작업하는 경우에도 일관된 원칙에 따라서 주석을 달아 놓으면 협업에서도 효율성이 증대됩니다.</P>
+<P>HTML을 공부하는 입장에서는 주석의 필요성을 잘 느끼지 못하지만 실무에서는 많이 사용합니다.</P></div>
+			</td>
+		</tr>
+		<tr>
+			<td style="padding:10px 10px 10px 20px;">
+				<div style="width:700px;"></div>
+								
+								
+			</td>
+		</tr>
+						<tr height="5">
+			<td bgcolor="#D1D8FF"></td>
+		</tr>
+		<tr height="120">
+			<td><table width="710" cellpadding="0" cellspacing="0" border="0" align="center">
+<tr><td colspan="3" height="10"></td></tr>
+<tr>
+	<td width="350"><script type="text/javascript"><!--
+google_ad_client = "pub-5161913013844597";
+//336x280, homejjang. 하단
+google_ad_slot = "5410558573";
+google_ad_width = 336;
+google_ad_height = 280;
+//--></script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></td>
+	<td width="10"></td>
+	<td width="350"><script type="text/javascript"><!--
+google_ad_client = "pub-5161913013844597";
+//336x280, homejjang. 하단
+google_ad_slot = "5410558573";
+google_ad_width = 336;
+google_ad_height = 280;
+//--></script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></td>
+</tr>
+<tr><td colspan="3" height="10"></td></tr>
+</table></td>
+		</tr>
+				</table>
+
+
+
+	
+			</td>
+</tr>
+<tr height="5">
+	<td colspan="4"><!-- margin --></td>
+</tr>
+</table>
+<table width="935" cellpadding="0" cellspacing="0" border="0" align="center">
+<tr>
+	<td style="color:#000000; padding:10px; width:935" align="center">Copyright &copy; Homejjang.com All rights reserved.</td>
+</tr>
+</table>
+</body>
+</html>
